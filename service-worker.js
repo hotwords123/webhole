86e2cc6273b13fd1d6e1c53b32aa9f0b689c5294
hotwords123 <31656766+hotwords123@users.noreply.github.com>
2021-09-01 09:50:40 +0000

    Deploying to gh-pages-master from  @ 7b24e6ea67144ade0ffa2e6cbcbf1e77570ac967 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox/workbox-sw.js");
 
 importScripts(
-  "https://cdn.jsdelivr.net/gh/treehollow/webhole@gh-pages-master/precache-manifest.3309145b645301b7c0804e2a59dbf7c5.js"
+  "https://cdn.jsdelivr.net/gh/hotwords123/webhole@gh-pages-master/precache-manifest.1f56feae8ee61f7a9b3fd6607398ffd1.js"
 );
 
 self.addEventListener('message', (event) => {
